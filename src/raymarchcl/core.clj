5ead561066044762c28a4c567b740f969cbaf4b4
Karsten Schmidt <k@postspectacular.com>
2013-03-03 12:49:27 +0000

    adding material presets, tweaking eps, delta & AO render params, adding sky gradient colours, adding load-volume fn, adding compute-eyepos

--- src/raymarchcl/core.clj
+++ src/raymarchcl/core.clj
@@ -5,7 +5,8 @@
    [simplecl.ops :as ops]
    [structgen.core :as sg]
    [structgen.parser :as sp]
-   [piksel.core :as pix]))
+   [piksel.core :as pix]
+   [clojure.java.io :as io]))
 
 (def cl-program
   "Location of the OpenCL program"
@@ -25,52 +26,73 @@
                  m (/ 1.0 (Math/sqrt (+ (* x x) (* y y) (* z z) (* w w))))]]
        [(* x m) (* y m) (* z m) (* w m)]))))
 
+(defn compute-eyepos
+  [theta rad y]
+  (let [theta (Math/toRadians theta)]
+    [(* rad (Math/cos theta)) y (* rad (Math/sin theta))]))
+
+(def material-presets
+  {:orange-stripes
+   {:lightColor [56 36 16]
+    :materials [{:albedo [1.0 1.0 1.0 1.0] :r0 0.4 :smoothness 0.9}
+                {:albedo [4.9 0.9 0.01 1.0] :r0 0.1 :smoothness 0.8}
+                {:albedo [1.9 1.9 1.9 1.0] :r0 0.3 :smoothness 0.4}
+                {:albedo [0.01 0.01 0.01 1.0] :r0 0.1 :smoothness 0.4}]
+    :aoAmp 0.2}
+   :ao
+   {:lightColor [50 50 50]
+    :materials [{:albedo [1.0 1.0 1.0 1.0] :r0 0.0 :smoothness 1.0}
+                {:albedo [1.0 1.0 1.0 1.0] :r0 0.0 :smoothness 1.0}
+                {:albedo [1.0 1.0 1.0 1.0] :r0 0.0 :smoothness 1.0}
+                {:albedo [1.0 1.0 1.0 1.0] :r0 0.0 :smoothness 1.0}]
+    :aoAmp 0.25}})
+
 (defn render-options
-  [{:keys [width height vres t iter eyepos]}]
-  (let [d 0.005]
-    {:resolution [width height]
-     :flareAmp 0.05
-     :materials
-     [{:albedo [1.0 1.0 1.0 1.0] :r0 0.4 :smoothness 0.1}
-      {:albedo [4.9 0.9 0.01 1.0] :r0 0.1 :smoothness 0.8}
-      {:albedo [1.9 1.9 1.9 1.0] :r0 0.3 :smoothness 0.4}
-      {:albedo [0.01 0.01 0.01 1.0] :r0 0.1 :smoothness 0.4}]
-     :maxDist 30
-     :invAspect (float (/ height width))
-     :eps 0.005
-     :skyColor [1.8 1.8 1.8]
-     :startDist 0.0
-     :isoVal 0.125
-     :voxelRes vres
-     :maxVoxelIter 128
-     :lightPos [2 3.0 2]
-     :shadowBias 0.1
-     :aoAmp 0.2
-     :voxelBoundsMin [-0.97 -0.97 -0.97]
-     :aoStepDist 0.2
-     :eyePos (or eyepos [1.2 0.15 1.2])
-     :aoIter 5
-     :voxelSize 0.05
-     :normOffsets [[d (- d) (- d) 0] [(- d) (- d) d 0] [(- d) d (- d) 0] [d d d 0]]
-     :frameBlend (max 0.01 (/ 1.0 iter))
-     :groundY 1.05
-     :shadowIter 80
-     :lightColor [56 36 16]
-     :targetPos [0 0 0]
-     :maxIter 128
-     :dof 0.025
-     :exposure 3.5
-     :minLightAtt 0.0
-     :voxelBounds2 [2 2 2]
-     :time t
-     :fogDensity 0.01
-     :voxelBoundsMax [0.97 0.97 0.97]
-     :lightScatter 0.2
-     :invVoxelScale [0.5 0.5 0.5]
-     :up [0 1 0]
-     :voxelBounds [1 1 1]
-     :gamma 1.5
-     :mcSamples (montecarlo 0x4000)}))
+  [{:keys [width height vres t iter eyepos mat]}]
+  (let [eps 0.005
+        d (* eps 1.6666)
+        clip 0.99]
+    (merge
+     {:resolution [width height]
+      :flareAmp 0.05
+      :maxDist 30
+      :invAspect (float (/ height width))
+      :eps eps
+      :skyColor1 [0.5 1.5 3.0]
+      :skyColor2 [1.8 1.8 1.8]
+      :startDist 0.0
+      :isoVal 0.125
+      :voxelRes vres
+      :maxVoxelIter 128
+      :lightPos [2 3.0 2]
+      :shadowBias 0.1
+      :aoAmp 0.05
+      :voxelBoundsMin [(- clip) (- clip) (- clip)]
+      :aoStepDist 0.05
+      :eyePos (or eyepos [2 0 2])
+      :aoIter 10
+      :voxelSize 0.05
+      :normOffsets [[d (- d) (- d) 0] [(- d) (- d) d 0] [(- d) d (- d) 0] [d d d 0]]
+      :frameBlend (max 0.01 (/ 1.0 iter))
+      :groundY 1.02
+      :shadowIter 128
+      :lightColor [50 50 50]
+      :targetPos [0 0 0]
+      :maxIter 128
+      :dof 0.015
+      :exposure 3.5
+      :minLightAtt 0.0
+      :voxelBounds2 [2 2 2]
+      :time t
+      :fogDensity 0.01
+      :voxelBoundsMax [clip clip clip]
+      :lightScatter 0.2
+      :invVoxelScale [0.5 0.5 0.5]
+      :up [0 1 0]
+      :voxelBounds [1 1 1]
+      :gamma 1.5
+      :mcSamples (montecarlo 0x4000)}
+     (get material-presets mat (material-presets :ao)))))
 
 (defn gyroid [s t p o]
   "Evaluates gyroid function at scaled point `v`."
@@ -84,14 +106,28 @@
 (defn make-volume
   [{[rx ry rz] :vres}]
   (prn "volume " rx ry rz)
-  (let [voxels (vec
-                (for [z (range rz) y (range ry) x (range rx)
-                      :let [v (gyroid 0.03 1.0 [x y z] [0.3875 0 0])]]
-                  (if (< (Math/abs (- 0.2 v)) 0.05)
-                    (if (< (rem x 32) 16) 0.25 0.5)
-                    (if (> v 0.35) 1 0))))]
-    ;;(prn "voxels:" (take 100 voxels))
+  (let [voxels (apply vector-of :float
+                      (for [z (range rz) y (range ry) x (range rx)]
+                        (if (< (bit-and z 0x0f) 8)
+                          0.0
+                          (let [v (gyroid 0.02625 1.0 [x y z] [0.3875 0.0 0.0])]
+                            (if (< (Math/abs (- 0.2 v)) 0.05)
+                              (if (< (bit-and x 0x1f) 16) 0.25 0.5)
+                              (if (> v 0.35) 1.0 0.0))))))]
     voxels))
+
+(defn load-volume
+  [path]
+  (with-open [in (java.io.DataInputStream. (io/input-stream path))]
+    (.read in (byte-array 5) 0 5) ; magic: VOXEL
+    (let [x (.readInt in) ; resx
+          y (.readInt in) ; resy
+          z (.readInt in) ; resz
+          s (.readByte in)
+          buf (cl/make-buffer :float (* x y z) :readonly)] ; element size in bytes
+      (while (pos? (.remaining buf))
+        (.put buf (float (if (pos? (.readByte in)) 0.25 0.0))))
+      (cl/rewind buf))))
 
 (defn make-pipeline
   [{:keys [o-buffers v-buf p-buf q-buf num] :as args}]
@@ -122,7 +158,9 @@
   (let [t-opts (sg/lookup :TRenderOptions)]
     (vec
      (for [i (range n)]
-       (cl/as-clbuffer (sg/encode t-opts (render-options (assoc opts :t (* i 0.333)))) :readonly)))))
+       (cl/as-clbuffer
+        (sg/encode t-opts (render-options (assoc opts :t (* i 0.333))))
+        :readonly)))))
 
 (defn update-option-buffers
   [buffers opts]
@@ -141,7 +179,7 @@
     (cl/with-state cl-state
       (println "build log: ")
       (println :cpu (cl/build-log (:program cl-state) (cl/max-device (:ctx cl-state) :cpu)))
-      (println :gpu (cl/build-log (:program cl-state) (cl/max-device (:ctx cl-state) :gpu)))
+      ;;(println :gpu (cl/build-log (:program cl-state) (cl/max-device (:ctx cl-state) :gpu)))
       (let [num (* width height)
             state (time
                    (merge
@@ -152,18 +190,22 @@
                      1 1
                      :v-buf {:wrap (make-volume args) :type :float :usage :readonly}
                      :p-buf {:size (* num 4) :type :float :usage :readwrite}
-                     :q-buf {:size num :type :int :usage :writeonly})))]
+                     :q-buf {:size num :type :int :usage :writeonly})
+                    ;;{:v-buf (load-volume "../toxi2/voxel-d8.vox")}
+                    ))]
         (assoc state :pipeline (make-pipeline state))))))
 
 (defn test-render
-  [width height iter res]
-  (let [state (init-renderer {:width width :height height :vres [res res res] :iter iter})]
+  [width height iter res mat]
+  (let [state (init-renderer {:width width :height height
+                              :vres [res res res]
+                              :iter iter
+                              :eyepos (compute-eyepos 45 2.25 0.25)
+                              :mat mat})]
     (cl/with-state (:cl-state state)
       (let [argb (time (ops/execute-pipeline (:pipeline state) :verbose false :final-size (:num state)))
             img (pix/make-image width height)
             img-pix (pix/get-pixels img)]
-        ;;(prn (apply str (map #(format "%08x " %) (take width (cl/buffer-seq argb)))))
-        ;;(cl/rewind argb)
         (prn "copying buffer")
         (loop [cols (cl/buffer-seq argb) i 0]
           (when (< i (:num state))
@@ -187,14 +229,11 @@
                frame-args (assoc args
                             :eyepos [(* 1.5 (Math/cos theta)) 0.15 (* 1.5 (Math/sin theta))])
                _ (update-option-buffers (:o-buffers state) frame-args)
-               ;;_ (prn state)
                argb (time (ops/execute-pipeline
                            (make-pipeline state)
                            :verbose false
                            :release false
                            :final-size (:num state)))]
-           ;;(prn (apply str (map #(format "%08x " %) (take width (cl/buffer-seq argb)))))
-           ;;(cl/rewind argb)
            (prn "copying pixel buffer")
            (loop [cols (cl/buffer-seq argb) i 0]
              (when (< i (:num state))
@@ -204,6 +243,13 @@
            (pix/save-png img (format "anim-%03d.png" frame)))))
       (cl/release (:ctx (:cl-state state))))))
 
-(defn rand2
-  [seed]
-  (bit-and (long (+ (* (bit-and seed 0xffffff) 0x5deece66d) 11)) (long 0xffffffff)))
+(defn trilin
+  [v000 v001 v010 v011 v100 v101 v110 v111 fx fy fz]
+  (let [mix #(+ % (* (- %2 %) %3))
+        vx (mix v000 v100 fx)
+        vy (mix v001 v101 fx)
+        vz (mix v010 v110 fx)
+        vw (mix v011 v111 fx)
+        i1 (mix vx vz fy)
+        i2 (mix vy vw fy)]
+    (mix i1 i2 fz)))
