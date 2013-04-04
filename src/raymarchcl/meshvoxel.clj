f4a49b6e5815e835a5c504432a59d716894024a1
Karsten Schmidt <k@postspectacular.com>
2013-04-05 00:57:08 +0100

    adding aws-install script & meshvoxel ns
    --HG--
    branch : normref

---
+++ src/raymarchcl/meshvoxel.clj
@@ -0,0 +1,86 @@
+(ns raymarchcl.meshvoxel
+  (:require
+   [raymarchcl.core :as rm]
+   [toxi.geom.core :as g]
+   [toxi.geom.meshio :as mio]
+   [clojure.java.io :as io]
+   [piksel.core :as pix]))
+
+(defn load-mesh
+  [path]
+  (-> path io/input-stream mio/read-stl))
+
+(defn mesh-scale
+  [vertices res]
+  (let [{p :p [sx sy sz] :size} (g/bounding-box* vertices)
+        md (max sx sy sz)
+        off (vec (map #(* 0.5 res (- 1.0 (/ % md))) [sx sy sz]))
+        s (g/vec3 (/ res md))]
+    (prn :p p :s [sx sy sz] :off off :md md :scale s)
+    (fn [v] (g/add3 off (g/scale3 (g/sub3 v p) s)))))
+
+(defn voxelize-scatter
+  [vertices res]
+  (let [voxels (byte-array (* res res res))
+        rxy (* res res)
+        r2 (/ res 2)
+        scale-fn (mesh-scale vertices res)]
+    (doseq [v vertices]
+      (let [[x y z] (map int (scale-fn v))]
+        (doseq [i (range (if (< (rand) 0.25) (rand 5) 1))]
+          (let [dx (int (rand (* (/ i 5) r2)))
+                x (int (- x dx (* res -0.4)))
+                z (max (- z (int (* r2 (+ (* 0.125 (rand)) 0.125)))) 0)
+                y (+ y (* res 0.4))]
+            (doseq [z (range (dec z) (+ 2 z))
+                    y (range (dec y) (+ 2 y))
+                    x (range (dec x) (+ 2 x))]
+              (when (and (>= z 0) (< z res) (>= y 0) (< y res) (>= x 0) (< x res))
+                (aset-byte voxels (+ (* (int y) rxy) (* (int z) res) (int x)) 64)))))))
+    voxels))
+
+(defn voxelize-ks
+  [vertices res ks]
+  (let [voxels (byte-array (* res res res))
+        rxy (* res res)
+        scale-fn (mesh-scale vertices res)
+        ks1 (inc ks)]
+    (doseq [v vertices]
+      (let [[x y z] (map int (scale-fn v))]
+        ;;(prn x y z)
+        (doseq [z (range (max 0 (- z ks)) (min res (+ z ks1)))
+                y (range (max 0 (- y ks)) (min res (+ y ks1)))
+                x (range (max 0 (- x ks)) (min res (+ x ks1)))]
+          (aset-byte voxels (+ (* (int z) rxy) (* (int y) res) (int x)) -1))))
+    voxels))
+
+(defn voxelize
+  [vertices res]
+  (let [voxels (byte-array (* res res res))
+        rxy (* res res)
+        scale-fn (mesh-scale vertices res)]
+    (doseq [v vertices]
+      (let [[x y z] (map int (scale-fn v))]
+        (when (and (>= z 0) (< z res) (>= y 0) (< y res) (>= x 0) (< x res))
+          (aset-byte voxels (+ (* (int z) rxy) (* (int y) res) (int x)) -1))))
+    voxels))
+
+(defn make-heatmap
+  [path amp]
+  (let [img (pix/load-image path)
+        pixels (pix/get-pixels img)
+        res (.getWidth img)
+        voxels (byte-array (* res res res))
+        rxy (* res res)]
+    (doseq [y (range res) x (range res)]
+      (let [c (bit-and (aget pixels (+ (* y res) x)) 255)
+            h (if (pos? c) (if (> c 224) 2 (max 2 (* c amp))) 0)]
+        (doseq [hh (range h)]
+          (aset-byte voxels (+ (* y rxy) (* hh res) x) -1))))
+    voxels))
+
+(defn make-heatmap-anim
+  [path out-path n]
+  (doseq [i (range n)]
+    (let [out (format out-path i)]
+      (rm/save-volume out 256 (make-heatmap path (float (/ i (* n 1.33333))))))))
